6d41ba3f8908ffaed3197d65265b5f27e7f32944
Hoang Dong <pidangkap@gmail.com>
2025-03-19 01:48:43 +0700

    dong làm Xac Nhan Dang ky

--- GUI_QuanLyThuVien/Program.cs
+++ GUI_QuanLyThuVien/Program.cs
@@ -18,7 +18,7 @@ namespace GUI_QuanLyThuVien
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new GUI_DangNhap());
+            Application.Run(new GUI_XacNhanOTP());
         }
     }
 }
--- GUI_QuanLyThuVien/User/GUI_XacNhanOTP.Designer.cs
+++ GUI_QuanLyThuVien/User/GUI_XacNhanOTP.Designer.cs
@@ -29,75 +29,48 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(GUI_XacNhanOTP));
             this.label1 = new System.Windows.Forms.Label();
             this.txtOTP = new System.Windows.Forms.TextBox();
             this.btnNext = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
             this.lbtimer = new System.Windows.Forms.Label();
-            this.btnclose = new System.Windows.Forms.Button();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(17, 21);
+            resources.ApplyResources(this.label1, "label1");
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(244, 16);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Vui lòng nhập mã OTP để xác nhận mail";
             // 
             // txtOTP
             // 
-            this.txtOTP.Location = new System.Drawing.Point(20, 53);
+            resources.ApplyResources(this.txtOTP, "txtOTP");
             this.txtOTP.Name = "txtOTP";
-            this.txtOTP.Size = new System.Drawing.Size(241, 22);
-            this.txtOTP.TabIndex = 1;
             // 
             // btnNext
             // 
-            this.btnNext.Location = new System.Drawing.Point(317, 52);
+            resources.ApplyResources(this.btnNext, "btnNext");
+            this.btnNext.DialogResult = System.Windows.Forms.DialogResult.Cancel;
             this.btnNext.Name = "btnNext";
-            this.btnNext.Size = new System.Drawing.Size(75, 23);
-            this.btnNext.TabIndex = 2;
-            this.btnNext.Text = "Next";
             this.btnNext.UseVisualStyleBackColor = true;
             this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
             // 
             // panel1
             // 
+            resources.ApplyResources(this.panel1, "panel1");
             this.panel1.Controls.Add(this.lbtimer);
-            this.panel1.Controls.Add(this.btnclose);
             this.panel1.Controls.Add(this.label1);
             this.panel1.Controls.Add(this.btnNext);
             this.panel1.Controls.Add(this.txtOTP);
-            this.panel1.Location = new System.Drawing.Point(28, 12);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(467, 145);
-            this.panel1.TabIndex = 3;
             // 
             // lbtimer
             // 
-            this.lbtimer.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.lbtimer.AutoSize = true;
+            resources.ApplyResources(this.lbtimer, "lbtimer");
             this.lbtimer.ForeColor = System.Drawing.Color.Red;
-            this.lbtimer.Location = new System.Drawing.Point(20, 78);
             this.lbtimer.Name = "lbtimer";
-            this.lbtimer.Size = new System.Drawing.Size(241, 16);
-            this.lbtimer.TabIndex = 8;
-            this.lbtimer.Text = "mã OTP của bạn có hiệu lực trong 120 s";
-            // 
-            // btnclose
-            // 
-            this.btnclose.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
-            this.btnclose.Location = new System.Drawing.Point(428, 3);
-            this.btnclose.Name = "btnclose";
-            this.btnclose.Size = new System.Drawing.Size(36, 23);
-            this.btnclose.TabIndex = 7;
-            this.btnclose.Text = "X";
-            this.btnclose.UseVisualStyleBackColor = false;
-            this.btnclose.Click += new System.EventHandler(this.btnclose_Click);
             // 
             // timer1
             // 
@@ -105,14 +78,14 @@
             // 
             // GUI_XacNhanOTP
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AcceptButton = this.btnNext;
+            resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(538, 172);
+            this.CancelButton = this.btnNext;
             this.Controls.Add(this.panel1);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Name = "GUI_XacNhanOTP";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "GUI_XacNhapOTP";
             this.Load += new System.EventHandler(this.GUI_XacNhanOTP_Load);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
@@ -126,7 +99,6 @@
         private System.Windows.Forms.TextBox txtOTP;
         private System.Windows.Forms.Button btnNext;
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Button btnclose;
         private System.Windows.Forms.Label lbtimer;
         private System.Windows.Forms.Timer timer1;
     }
